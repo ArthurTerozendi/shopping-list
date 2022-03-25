d84363bb359e6f2cc31476d81e32eb2d9575204b
Arthur Terozendi <arthurterozendi@gmail.com>
2022-03-24 21:20:47 -0300

    Initial routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full"
+  },
+  {
+    path: "home",
+    loadChildren: () => import("./pages/home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: ":id/list",
+    loadChildren: () => import("./pages/home/home.module").then(m => m.HomeModule)
+  },
+  // {
+  //   path: "**",
+  //   component: NotFound
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
