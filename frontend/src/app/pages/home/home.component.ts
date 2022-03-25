ec109fdd97a28c604d3c84b82db13628040d12da
Arthur Terozendi <arthurterozendi@gmail.com>
2022-03-24 22:01:17 -0300

    Structuring home

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  aux: string[] = ["", "", "", "", "", ""]
+
   constructor() { }
 
   ngOnInit(): void {
