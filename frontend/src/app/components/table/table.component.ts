ef6af031714d1a02333f81bfd115299a66bedc4c
Arthur Terozendi <arthurterozendi@gmail.com>
2022-05-01 11:32:06 -0300

    Edit and emit a cell edit

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { TableInfo } from './table-info.model';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { EventResponse, TableInfo } from './table-info.model';
 
 @Component({
   selector: 'app-table',
@@ -9,10 +9,28 @@ import { TableInfo } from './table-info.model';
 export class TableComponent implements OnInit {
 
   @Input() tableInfo: TableInfo = { head: [], bodyRow: [] };
+  @Output() editCellEvent: EventEmitter<EventResponse> = new EventEmitter<EventResponse>()
 
   constructor() { }
+  edit: boolean[][] = [[false]];
 
   ngOnInit(): void {
+    this.tableInfo.bodyRow.forEach(r => {
+      let row: boolean[] = [];
+      Object.values(r).forEach(c => row.push(false))
+      this.edit.push(row);
+    })
   }
 
+  toggleCellEditing(row: number, column: number) {
+    this.edit[row][column] = !this.edit[row][column];
+  }
+
+  onEditCell(row: number, column: number, value: string) {
+    this.toggleCellEditing(row, column);
+    this.tableInfo.bodyRow[row].bodyInfo[column].name = value;
+  }
+  emitEvent(row: number, column: number, value: string) {
+    this.editCellEvent.emit({ row: row, column: column, value: value });
+  }
 }
